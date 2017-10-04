d245fbb8f460c77002cc1c4d5549a1b832dd07a6
shiny solutions <shinysolutions16@gmail.comdsd>
2017-10-05 01:25:33 +0530

    Screen # 3 Dashboard changes

--- app/src/main/java/com/hiulatam/hiu/hiu/DashboardActivity.java
+++ app/src/main/java/com/hiulatam/hiu/hiu/DashboardActivity.java
@@ -1,10 +1,119 @@
 package com.hiulatam.hiu.hiu;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.hiulatam.hiu.hiu.common.Config;
 
 /**
- * Created by pranshi on 9/30/17.
+ * Created by: Shiny Solutions
+ * Created on: 9/30/17
  */
 
 public class DashboardActivity extends AppCompatActivity {
+
+    public static final String TAG = "DashboardActivity - ";
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    LinearLayout linearLayoutTitle;
+    ImageButton imageButtonNavigationView;
+    SearchView searchViewCelebrity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        bindComponents();
+        init();
+        addListeners();
+    }
+
+    /**
+     * Created by: Shiny Solutions
+     * Created on: 9/30/17
+     */
+    public void bindComponents(){
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        linearLayoutTitle = (LinearLayout) findViewById(R.id.linear_layout_title);
+
+        imageButtonNavigationView = (ImageButton) findViewById(R.id.image_button_navigation_view);
+
+        searchViewCelebrity = (SearchView) findViewById(R.id.search_view_celebrity);
+    }
+
+    /**
+     * Created by: Shiny Solutions
+     * Created on: 9/30/17
+     */
+    public void init(){
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+    }
+
+    /**
+     * Created by: Shiny Solutions
+     * Created on: 9/30/17
+     */
+    public void addListeners(){
+        imageButtonNavigationView.setOnClickListener(onClickListener);
+        searchViewCelebrity.setOnSearchClickListener(onClickListener);
+        searchViewCelebrity.setOnCloseListener(onCloseListener);
+    }
+
+    /**
+     * Created by: Shiny Solutions
+     * Created on: 10/03/17
+     */
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.image_button_navigation_view:
+                    drawerLayout.openDrawer(Gravity.LEFT);
+                    break;
+                case R.id.search_view_celebrity:
+                    imageButtonNavigationView.setVisibility(View.GONE);
+                    linearLayoutTitle.setVisibility(View.GONE);
+                    break;
+            }
+        }
+    };
+
+    SearchView.OnCloseListener onCloseListener = new SearchView.OnCloseListener() {
+        @Override
+        public boolean onClose() {
+            imageButtonNavigationView.setVisibility(View.VISIBLE);
+            linearLayoutTitle.setVisibility(View.VISIBLE);
+            return false;
+        }
+    };
+
+
+
 }
--- app/src/main/java/com/hiulatam/hiu/hiu/LoginActivity.java
+++ app/src/main/java/com/hiulatam/hiu/hiu/LoginActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.an.customfontview.CustomButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity - ";
+
+    CustomButton buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,56 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bindComponents();
+        init();
+        addListeners();
     }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  09/30/2017
+     */
+    private void bindComponents(){
+        buttonSignIn = (CustomButton) findViewById(R.id.sign_in);
+
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  09/30/2017
+     */
+    private void init(){
+
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  09/30/2017
+     */
+    private void addListeners(){
+        buttonSignIn.setOnClickListener(clickListener);
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  09/30/2017
+     */
+    private void showDashboard(){
+        Intent intent = new Intent();
+        intent.setClass(this, DashboardActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  09/30/2017
+     */
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            showDashboard();
+        }
+    };
 }
