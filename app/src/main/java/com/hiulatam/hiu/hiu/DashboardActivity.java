c841d848fdc90f8e80980fb8af34b83a87412246
Pranshi <pranshi@Pranshis-MacBook-Air.local>
2017-10-05 01:17:13 +0530

    Screen # 3 Dashboard changes

---
+++ app/src/main/java/com/hiulatam/hiu/hiu/DashboardActivity.java
@@ -0,0 +1,10 @@
+package com.hiulatam.hiu.hiu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pranshi on 9/30/17.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/hiulatam/hiu/hiu/common/Config.java
@@ -0,0 +1,13 @@
+package com.hiulatam.hiu.hiu.common;
+
+import android.nfc.Tag;
+
+/**
+ * Created by:  Shiny Solutions
+ * Created on:  10/4/17.
+ */
+
+public class Config {
+
+    public static final String Tag = "com.hiulatam.hiu.hiu";
+}
