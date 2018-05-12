2b11944cc61cdfc61213dbbe9851524c85d94253
shiny solutions <shinysolutions16@gmail.comdsd>
2018-05-12 08:34:14 +0530

    Suggested changes

---
+++ app/src/main/java/com/hiulatam/hiu/hiu/fragments/PaymentConfirmationDialogFragment.java
@@ -0,0 +1,10 @@
+package com.hiulatam.hiu.hiu.fragments;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by pranshi on 5/12/18.
+ */
+
+public class PaymentConfirmationDialogFragment extends DialogFragment {
+}
